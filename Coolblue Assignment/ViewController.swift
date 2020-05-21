5b70fa225937baef72b4f2f4d4f59ceb02d691bd
Klaas Schoenaker <klaasscho@gmail.com>
2020-05-21 13:20:09 +0200

    Initial Commit

---
+++ Coolblue Assignment/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Coolblue Assignment
+//
+//  Created by Klaas Schoenmaker on 5/21/20.
+//  Copyright © 2020 Klaas Schoenmaker. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view.
+	}
+
+
+}
+
